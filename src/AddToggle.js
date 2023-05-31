2599cf994fdb6bf6f3c6fcfd149b00d4ac77251e
jeongjwon <zlzlsksk111@gmail.com>
2023-05-31 20:58:50 +0900

    feat. 추가하기 버튼 컴포넌트 생성

---
+++ src/AddToggle.js
@@ -0,0 +1,77 @@
+import { useCallback, useState } from "react";
+import useInput from "./hooks/useInput";
+import {
+  AddBackground,
+  AddForm,
+  FormWrapper,
+  SubmitBtn,
+  CloseBtn,
+  BtnZone,
+} from "./AddToggle.styled";
+
+const AddToggle = ({ onInsert, handleOpenModal }) => {
+  const [date, dateBind] = useInput("");
+  const [distance, distanceBind] = useInput("0.00");
+  const [hour, hourBind] = useInput("0");
+  const [minute, minuteBind] = useInput("00");
+  const [second, secondBind] = useInput("00");
+  const [perMin, perMinBind] = useInput("0");
+  const [perSec, perSecBind] = useInput("00");
+
+  const handleSumbit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const newRecord = {
+        id: Date.now(),
+        date,
+        distance,
+        time: `${hour} : ${minute} : ${second}`,
+        pace: `${perMin} : ${perSec},`,
+      };
+      console.log(newRecord);
+      onInsert(newRecord);
+      handleOpenModal();
+    },
+    [onInsert, date, distance, hour, minute, second, perMin, perSec]
+  );
+
+  return (
+    <AddBackground>
+      <AddForm onSubmit={handleSumbit}>
+        <FormWrapper>
+          <div className="item">
+            <span>날짜</span>
+            <input type="date" {...dateBind} required></input>
+          </div>
+          <div className="item">
+            <span>거리</span>
+            <input
+              type="number"
+              {...distanceBind}
+              min={0}
+              step="0.01"
+              required
+            />
+          </div>
+          <div className="item">
+            <span>시간</span>
+            <input type="number" {...hourBind} min={0} />:
+            <input type="number" {...minuteBind} min={0} max={59} required /> :
+            <input type="number" {...secondBind} min={0} max={59} required />
+          </div>
+          <div className="item">
+            <span>평균 페이스</span>
+            <input type="number" {...perMinBind} min={0} required /> :
+            <input type="number" {...perSecBind} min={0} max={59} required />
+          </div>
+        </FormWrapper>
+        <BtnZone>
+          <CloseBtn onClick={handleOpenModal}>닫기</CloseBtn>
+          <SubmitBtn type="submit">제출하기</SubmitBtn>
+        </BtnZone>
+      </AddForm>
+    </AddBackground>
+  );
+};
+
+export default AddToggle;
