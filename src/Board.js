37d0285a9799b7dd6333070b65bfc8c1e4297b62
jeongjwon <zlzlsksk111@gmail.com>
2023-05-31 20:58:00 +0900

    feat. Board 컴포넌트 생성

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,30 @@
+import AddToggle from "./AddToggle";
+import RunningList from "./RunningList";
+import { useState } from "react";
+import { BoardContainer, AddBtn } from "./Board.styled";
+import useLocalStorage from "./hooks/useLocalStorage";
+
+
 function Board() {
-    return (
-      <div className="container">
-        
-      </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+  const [records, setRecords] = useLocalStorage('records', []);
+
+  const onInsert = (record) => {
+    setRecords((prevState) => [...prevState, record]);
   }
-  
-  export default Board;
+  const handleOpenModal = () => {
+    setIsOpen(!isOpen);
+  }
+  console.log(records);
+  return (
+    <BoardContainer>
+      <AddBtn onClick={handleOpenModal}>추가하기</AddBtn>
+      <RunningList 
+      records={records} />
+      {isOpen && <AddToggle onInsert={onInsert} handleOpenModal={handleOpenModal}/>}
+
+    </BoardContainer>
+  );
+}
+
+export default Board;
