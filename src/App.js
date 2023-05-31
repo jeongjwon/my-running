b93995aaf8345625137f44f35c0588aabeb48ecf
jeongjwon <zlzlsksk111@gmail.com>
2023-05-31 20:59:30 +0900

    styled-component 로 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import "./App.css";
+// import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
+import styled from "styled-components";
 
+export const Container = styled.div`
+  background-color: rgba(0, 248, 125, 0.4);
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  padding: 5rem 8rem;
+`;
 function App() {
   return (
-    <div className="container">
+    <Container>
       <Header />
       <Main />
-    </div>
+    </Container>
   );
 }
 
