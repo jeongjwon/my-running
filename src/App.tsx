09a014082255185edcf73cab059dff61074ac106
jeongjwon <zlzlsksk111@gmail.com>
2023-09-19 19:13:19 +0900

    fix. 높이 조절

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import styled from "styled-components";
+
 import Header from "./components/Header";
 import Main from "./components/Main";
 function App() {
@@ -14,7 +15,7 @@ function App() {
 export const Container = styled.div`
   background-color: rgba(0, 248, 125, 0.4);
   position: relative;
-  height: 100vh;
+
   padding: 5rem 8rem;
 `;
 export default App;
